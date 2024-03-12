484985e8ba08e76b5ebcf07ac1aa56b7f9459c67
Ivan23BG <ilejeune@outlook.com>
2024-03-12 16:20:33 +0100

    avancement tp note

--- HAX606X-Optimisation-convexe/tps/tp3.py
+++ HAX606X-Optimisation-convexe/tps/tp3.py
@@ -3,28 +3,46 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import cm
 
+#%% Constantes
+# on utilisera sauf mention contraire les constantes suivantes
+gamma = 0.01
+maxiter = 1000
+epsilon = 1e-5
+
 #%%
-# Descente de gradients et variantes
-
-# Algorithme de la descente de gradient
-
 # Question 1 : Coder la descente de gradient
-# et garder la liste des itérés
+# avec en sortie la suite des x_k
+# ensuite tester l'algorithme sur la fonction f_test
 
+#%%
+# Algorithme de la descente de gradient
 def algo1(grad_f, x_init, gamma, maxiter, epsilon):
     x = x_init
-    x_list = [x]
+    x_k = [x]
     for i in range(maxiter):
         x = x - gamma * grad_f(x)
-        x_list.append(x)
+        x_k.append(x)
         if np.linalg.norm(grad_f(x)) < epsilon:
             break
-    return x, x_list
+    return x_k
 #%%
+# Fonction de test
 def f_test(x_1, x_2):
     return (x_1-1)**2 + 3*(x_2+1)**2
 
 #%%
+# gradient de f_test
+def grad_f_test(x):
+    return np.array([2*(x[0]-1), 6*(x[1]+1)])
+
+#%%
+# verification de l'algorithme sur la fonction f_test
+x_init = np.array([0, 0])
+x_min = algo1(grad_f_test, x_init, gamma, maxiter, epsilon)
+print(x_min[-1], "\n",x_min)
+
+#%%
+# visualisation de la fonction f_test
 fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
 X = np.arange(-5, 5, 0.25)
 Y = np.arange(-5, 5, 0.25)
@@ -39,76 +57,74 @@ fig.colorbar(surf, shrink=0.5, aspect=5)
 plt.show()
 
 #%%
-# Tester l'algorithme sur la fonction f_test
-def grad_f_test(x):
-    return np.array([2*(x[0]-1), 6*(x[1]+1)])
-
+# tester l'algorithme sur la fonction f_test
 x_init = np.array([0, 0])
 gamma = 0.01
 maxiter = 1000
 epsilon = 1e-5
 x_min = algo1(grad_f_test, x_init, gamma, maxiter, epsilon)
-print(x_min[0],"\n", x_min[1])
+print(x_min[-1],"\n", x_min)
 
 # %% Question 2 : Application au cas quadratique
 
 # On pose la fonction quadratique f_(a,b)(x,y)=y^2/a+x^2/b
-# tracer l'evolution graphique des f_(a,b)(x_k) dans l'algo
 # avec a=b et a=1, puis 10, 50, 100
-# f correspond a f_test
 
-def f_ab(a, b, x, y):
+#%%
+# fonction f_ab
+def f_ab(x, y):
+    global a, b
     if a > 0 and b > 0:
         return y**2/a + x**2/b
-    
+    else:
+        print("a et b doivent etre positifs")
+
+#%%
+# gradient de f_ab
 def grad_f_ab(X):
+    global a, b
     if a > 0 and b > 0:
         return np.array([2*X[0]/b, 2*X[1]/a])
 
-#%%
-gamma = 0.01
-maxiter = 1000
-epsilon = 1e-5
+#%% tester l'algorithme sur la fonction f_ab
 a = 1
 b = 1
 x_init = np.array([1, 1])
 x_min = algo1(grad_f_ab, x_init, gamma, maxiter, epsilon)
+print(x_min[-1],"\n", x_min)
 
 #%%
 # plot the evolution of f_ab(x_k)
-x_list = np.array(x_min[1])
-f_ab_list = f_ab(a, b, x_list[:,0], x_list[:,1])
+x_list = np.array(x_min)
+f_ab_list = f_ab(x_list[:,0], x_list[:,1])
 plt.plot(f_ab_list)
 
 #%%
 # plot on same graph the evolution of f_ab(x_k) for a=1, 10, 50, 100
 x_init = np.array([1, 1])
 a_list = [1, 10, 50, 100]
-gamma = 0.01
-maxiter = 1000
-epsilon = 1e-5
 for a in a_list:
     b=a
     x_min = algo1(grad_f_ab, x_init, gamma, maxiter, epsilon)
-    x_list = np.array(x_min[1])
-    f_ab_list = f_ab(a, b, x_list[:,0], x_list[:,1])
+    x_list = np.array(x_min)
+    f_ab_list = f_ab(x_list[:,0], x_list[:,1])
     # plot
     plt.yscale("log")
     plt.plot(f_ab_list, label="a="+str(a))
     # title
-    plt.title("Evolution of f_ab(x_k) for a=1, 10, 50, 100")
+    plt.title("Evolution des f en echelle log pour a=1, 10, 50, 100")
     # plot with legend
     plt.legend()
+    print(f"pour a = {a}:\t",x_min[-1],"\n", x_min)
 
-print(x_min[0],"\n", x_min[1])
-
-#%% plot the surface of f_ab
-
+#%% visualisation de la fonction f_ab pour a=1, b=1
+a = 1
+b = 1
 fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
 X = np.arange(-5, 5, 0.25)
 Y = np.arange(-5, 5, 0.25)
 X, Y = np.meshgrid(X, Y)
-Z = f_ab(a, b, X, Y)
+Z = f_ab(X, Y)
 
 # Plot the surface.
 surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm, linewidth=0, antialiased=False)
@@ -117,22 +133,150 @@ surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm, linewidth=0, antialiased=False
 fig.colorbar(surf, shrink=0.5, aspect=5)
 plt.show()
 
-#%% plot countour lines of f_ab
+#%% visualisation de la fonction f_ab pour a=100, b=100
+a = 100
+b = 100
+fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+X = np.arange(-5, 5, 0.25)
+Y = np.arange(-5, 5, 0.25)
+X, Y = np.meshgrid(X, Y)
+Z = f_ab(X, Y)
 
-plt.contour(X, Y, Z, 20, cmap='RdGy')
-plt.colorbar()
+# Plot the surface.
+surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+
+# Add a color bar which maps values to colors.
+fig.colorbar(surf, shrink=0.5, aspect=5)
 plt.show()
-
-#%% plot the evolution of x_k on the countour lines of f_ab
-
-# plot the evolution of f_ab(x_k) on different plots
+#%% plot countour lines of f_ab and points of x_k
+X = np.arange(-5, 5, 0.25)
+Y = np.arange(-5, 5, 0.25)
+X, Y = np.meshgrid(X, Y)
 for a in a_list:
     b=a
-    Z = f_ab(a, b, X, Y)
+    Z = f_ab(X, Y)
     plt.contour(X, Y, Z, 20)
     plt.colorbar()
     # title
-    plt.title("Evolution of f_ab(x_k) for a=1, 10, 50, 100")
-    plt.plot(x_list[:,0], x_list[:,1], label="a="+str(a))
-    plt.legend()
+    plt.title("Evolution of contour lines for f_ab")
+    # plot the evolution of f_ab(x_k)
+    plt.show()
     
+
+#%% on remarque pour les vitesses
+# de convergence que plus a est grand
+# plus la valeur initiale est proche de celle souhaitée
+# mais plus l'algorithme est lent
+
+#%%
+# Comparer sur un graphique la distance à l’optimum en norme l_2
+# pour chacun des cas avec une échelle logarithmique.
+
+
+
+#%% same problem with a != b
+# plot on same graph the evolution of f_ab(x_k) for a=1, 10, 50, 100
+x_init = np.array([1, 1])
+a=3
+b=0.5
+x_min = algo1(grad_f_ab, x_init, gamma, maxiter, epsilon)
+x_list = np.array(x_min)
+f_ab_list = f_ab(x_list[:,0], x_list[:,1])
+# plot
+plt.yscale("log")
+plt.plot(f_ab_list, label="a="+str(a))
+# title
+plt.title("Evolution des f en echelle log pour a=3, b=0.5")
+# plot with legend
+plt.legend()
+print(f"pour a = {a}:\t",x_min[-1],"\n", x_min)
+
+
+#%%
+# visualisation de la fonction f_ab pour a=3, b=0.5
+a = 3
+b = 0.5
+fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+X = np.arange(-5, 5, 0.25)
+Y = np.arange(-5, 5, 0.25)
+X, Y = np.meshgrid(X, Y)
+Z = f_ab(X, Y)
+
+# Plot the surface.
+surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+
+# Add a color bar which maps values to colors.
+fig.colorbar(surf, shrink=0.5, aspect=5)
+
+#%%
+# plot countour lines of f_ab and points of x_k
+a = 3
+b = 0.5
+X = np.arange(-5, 5, 0.25)
+Y = np.arange(-5, 5, 0.25)
+X, Y = np.meshgrid(X, Y)
+Z = f_ab(X, Y)
+plt.contour(X, Y, Z, 20)
+plt.colorbar()
+# title
+plt.title("Evolution of f_ab(x_k) for a=3, b=0.5")
+# plot the evolution of f_ab(x_k)
+x_init = np.array([1, 1])
+a=3
+b=0.5
+x_min = algo1(grad_f_ab, x_init, gamma, maxiter, epsilon)
+x_list = np.array(x_min)
+plt.plot(x_list[:,0], x_list[:,1], label="a="+str(a))
+plt.legend()
+
+
+#%%
+# Descente par coordonnée
+
+#%%
+# Question 3: Descente de gradient par coordonnée
+
+#%%
+# Question 3.1: Coder la methode de descente de 
+# gradient par coordonnées avec le même critère
+# d'arrêt que la descente de gradient classique
+
+def algo2(grad, x_init, gamma, n_iter, epsilon):
+    x = x_init
+    x_k = [x]
+    for i in range(1, n_iter+1):
+        for j in range(len(x)):
+            x[j] = x[j] - gamma * grad(x)[j]
+            if np.linalg.norm(grad(x)[j]) < epsilon:
+                break
+        x_k.append(x)
+        
+    return x_k
+
+
+#%%
+# Question 3.2: Comparer la descente de gradient
+# classique à la descente de gradient par coordonnées
+# pour la fonction f_ab avec differentes valeurs de a=b
+
+# plot la descente de gradient classique
+a = 1
+b = 1
+x_init = np.array([1, 1])
+x_min_1 = algo1(grad_f_ab, x_init, gamma, maxiter, epsilon)
+x_list_1 = np.array(x_min_1)
+f_ab_list_1 = f_ab(x_list_1[:,0], x_list_1[:,1])
+x_min_2 = algo2(grad_f_ab, x_init, gamma, maxiter, epsilon)
+x_list_2 = np.array(x_min_2)
+f_ab_list_2 = f_ab(x_list_2[:,0], x_list_2[:,1])
+# plot
+plt.yscale("log")
+plt.plot(f_ab_list_1, label="classique")
+plt.plot(f_ab_list_2, label="par coordonnee")
+
+# title
+plt.title("Evolution des x_k en echelle log pour a=1, b=1")
+# plot with legend
+plt.legend()
+print(f"pour a = {a}:\t",x_min_1[-1],"\n", x_min_1)
+
---
+++ HAX606X-Optimisation-convexe/tps/tp_note_HAX606X_Ivan_Lejeune.py
@@ -0,0 +1,101 @@
+# -----
+# TP note - descente de gradients et variantes
+
+# -----
+#%%
+# ----- Importation des bibliothèques
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+
+# -----
+#%% 
+# ----- Paramètres par défaut
+# On utilisera sauf mention contraire les paramètres suivants
+gamma = 0.01
+maxiter = 1000
+epsilon = 1e-5
+
+# -----
+#%%
+# ----- Question 1 - Coder la descente de gradient
+
+# ----- Adaptation de l'algorithme en python avec en sortie la suite des (x_k)
+def algo1(grad, x_init, gamma, maxiter, epsilon):
+    x = x_init
+    x_k = [x]
+    for i in range(1, maxiter + 1):
+        g = grad(x)
+        if np.linalg.norm(g) < epsilon ** 2:
+            break
+        else:
+            x = x - gamma * g
+            x_k.append(x)
+    return x_k
+
+# -----
+#%%
+# ----- Verification de l'algorithme sur une fonction simple
+
+# ----- Fonction de test
+def f_test(x, y):
+    return (x - 1) ** 2 + 3 * (y + 1) ** 2
+
+# ----- Gradient de la fonction de test
+def grad_f_test(x):
+    return np.array([2 * (x[0] - 1), 6 * (x[1] + 1)])
+
+# ----- Application de l'algorithme
+x_init = np.array([0, 0]) # Point initial différent de (1, -1)
+x_k = algo1(grad_f_test, x_init, gamma, maxiter, epsilon)
+print(f"Résultat de l'algorithme : {x_k[-1]}")
+
+# -----
+#%%
+# ----- Visualisation de la fonction
+fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+X = np.arange(-5, 5, 0.25)
+Y = np.arange(-5, 5, 0.25)
+X, Y = np.meshgrid(X, Y)
+Z = f_test(X, Y)
+surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+fig.colorbar(surf, shrink=0.5, aspect=5)
+ax.set_title("Fonction de test")
+ax.set_xlabel("x")
+ax.set_ylabel("y")
+ax.set_zlabel("f(x, y)")
+plt.show()
+
+# -----
+# ----- Fin Question 1
+#%%
+# ----- Question 2 - Application au cas quadratique
+
+# ----- Fonction quadratique
+def f_quad(x):
+    if A > 0 and B > 0:
+        return (x[1] ** 2) / A + (x[0] ** 2) / B
+    else:
+        raise ValueError("A et B doivent être strictement positifs")
+
+# ----- Initialisation des paramètres
+A = 1
+B = 1
+
+# ----- Gradient de la fonction quadratique
+def grad_f_quad(x):
+    return np.array([2 * x[0] / B, 2 * x[1] / A])
+
+# ----- Application de l'algorithme
+x_init = np.array([1, 1]) # Point initial différent de (0, 0)
+x_k = algo1(grad_f_quad, x_init, gamma, maxiter, epsilon)
+print(f"Résultat de l'algorithme : {x_k[-1]}")
+
+# -----
+#%%
+# ----- Question 2.1 - Evolution graphique des (x_k) de l'algorithme
+x_list = np.array(x_k)
+f_quad_list = f_quad(x_list.T)
+plt.plot(f_quad_list)
+
+# ----- Meme chose avec a = b = 10, 50, 100
